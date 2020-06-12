3a7142879d2ea638fd91cca281593b35708ca94e
Sergio Majé <s.maje@udla.edu.co>
2020-06-12 00:18:58 -0500

    Implementación de Logistic Classifier arquitectura

--- models/LogisticClassifier.py
+++ models/LogisticClassifier.py
@@ -15,4 +15,37 @@ class LogisticClassifier:
 
         self.y = tf.placeholder(shape=[None], dtype=tf.int32)
         self.y_one_hot = tf.one_hot(self.y, self.num_classes, dtype=tf.float32)
+
+        self.batch_size = tf.placeholder(tf.int32, [], name='batch_size')
+
+        # y = mx + b
+        weights = {
+            'layer_0': tf.Variable(tf.random_normal([
+                self.seq_max_len * self.vocab_size,  # capa de entrada
+                self.num_classes  # capa de salida
+            ]))
+        }
+        biases = {
+            'layer_0': tf.Variable(tf.random_normal([self.num_classes]))
+        }
+
+        x_input = tf.reshape(x_one_hot, [-1, self.seq_max_len * self.vocab_size])
+        output = tf.matmul(x_input, weights['layer_0']) + biases['layer_0']
+        self.logits = tf.sigmoid(output)
+        self.probs = tf.softmax(self.logits, axis=1)
+
+        self.correct_preds = tf.equal(
+            tf.argmax(self.probs, axis=1), 
+            tf.argmax(self.y_one_hot, axis=1)
+        )
+        self.precision = tf.reduce_mean(tf.cast(self.correct_preds, tf.float32))
+
+    def step_training(self, learning_rate=0.01):
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            logits=self.logits, 
+            labels=self.y_one_hot
+        ))
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+        
+        return loss, optimizer
         
