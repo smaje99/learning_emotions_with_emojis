097d13c607cca2dc67c07f2d5758aed6c26cf73f
Sergio Majé <s.maje@udla.edu.co>
2020-06-14 01:01:10 -0500

    Implementación de LSTM arquitectura y optimización

--- models/LSTM.py
+++ models/LSTM.py
@@ -21,6 +21,41 @@ class LSTM:
         self.y_one_hot = tf.one_hot(self.y, self.num_classes, dtype=tf.float32)
         self.batch_size = tf.placeholder(shape=[], dtype=tf.int32, name='batch_size')
 
-        
+        weights = {
+            'layer_0': tf.Variable(tf.random_normal([self.seq_max_len, 256])),
+            'layer_1': tf.Variable(tf.random_normal([256, self.num_classes]))
+        }
+        bias = {
+            'layer_0': tf.Variable(tf.random_normal([256])), 
+            'layer_1': tf.Variable(tf.random_normal([self.num_classes]))
+        }
 
+        init_state = tf.zeros([self.batch_size, self.state_size])
+        cell = tf.contrib.rnn.BasicLSTMCell(self.state_size)
+        self.outputs, self.final_state = tf.contrib.rnn.static_rnn(
+            cell, 
+            rnn_input, 
+            dtype=float32
+        )
+
+        output = self.outputs[-1]
+        hidden = tf.matmul(output, weights['layer_0']) + biases['layer_0']
+        hidden = tf.nn.tanh(hidden)
+        self.logits = tf.matmul(output, weights['layer_1']) + biases['layer_1']
+        self.probs = tf.nn.softmax(hidden)  # convetir varlores en probabilidades
+
+        self.correct_preds = tf.equal(
+            tf.argmax(self.probs, axis=1), 
+            tf.argmax(self.y_one_hot, axis=1)
+        )
+        self.precision = tf.reduce_mean(tf.cast(self.correct_preds, tf.float32))
+
+    def step_training(self, learning_rate=0.01):
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            logits=self.logits, 
+            labels=self.y_one_hot
+        ))
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+        
+        return loss, optimizer
        
